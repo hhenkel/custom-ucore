cbdb629e53a580a80fc882964caf09c9afd30317
Henning Henkel <henning+github@henkel.cx>
2026-04-06 16:58:13 +0200

    Add folder creation for dracut-sshd

--- build.sh
+++ build.sh
@@ -14,6 +14,7 @@ EOF
 curl -L https://copr.fedorainfracloud.org/coprs/gsauthof/dracut-sshd/repo/fedora-43/gsauthof-dracut-sshd-fedora-43.repo -o /etc/yum.repos.d/gsauthof-dracut-sshd-fedora-43.repo
 
 
+mkdir /etc/dracut-sshd
 echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIvhNf+pVcXIAvM/xe9IFj0157y3EQ1Z51DUUlzrG47C" > /etc/dracut-sshd/authorized_keys
 
 rpm-ostree install netbird dracut-sshd
