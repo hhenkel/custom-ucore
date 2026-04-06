96e477402c38b2ad8175b7906e2e9bf4f173cd2a
Henning Henkel <henning+github@henkel.cx>
2026-04-06 12:49:22 +0200

    Quick and dirty add pub key

--- build.sh
+++ build.sh
@@ -13,5 +13,9 @@ EOF
 
 curl -L https://copr.fedorainfracloud.org/coprs/gsauthof/dracut-sshd/repo/fedora-43/gsauthof-dracut-sshd-fedora-43.repo -o /etc/yum.repos.d/gsauthof-dracut-sshd-fedora-43.repo
 
+
+echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIvhNf+pVcXIAvM/xe9IFj0157y3EQ1Z51DUUlzrG47C" > /etc/dracut-sshd/authorized_keys
+
 rpm-ostree install netbird dracut-sshd
+
 dnf5 clean all
